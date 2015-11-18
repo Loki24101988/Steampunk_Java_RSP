bc5b551241fbf1a291c495d1eecd293d72b4bbe5
Loki <phalandra.mandragora@googlemail.com>
2015-11-18 11:07:10 +0100

    MasterView useless imports deleted

--- Steampunk_Game/src/team/tmbe/view/MasterView.java
+++ Steampunk_Game/src/team/tmbe/view/MasterView.java
@@ -1,28 +1,21 @@
 package team.tmbe.view;
 
-import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.scene.shape.MeshView;
 import javafx.stage.Stage;
 
-public class MasterView extends Application{
+public class MasterView extends Application {
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/MasterView.fxml"));
-        
-        primaryStage.setTitle("FXML Hello World!");
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
-        primaryStage.setScene(new Scene(root, 300, 250));
-        primaryStage.show();
-    }
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("fxml/MasterView.fxml"));
+
+		primaryStage.setTitle("FXML Hello World!");
+		setUserAgentStylesheet(STYLESHEET_CASPIAN);
+		primaryStage.setScene(new Scene(root, 300, 250));
+		primaryStage.show();
+	}
 
 }
