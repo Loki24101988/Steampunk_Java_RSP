f92648a36665a8ea5587598b35e0b78249b9249e
Loki <phalandra.mandragora@googlemail.com>
2015-11-19 15:33:05 +0100

    edited abstract class NonHuman

--- Steampunk_Game/src/team/tmbe/model/livingThing/nonHuman/NonHuman.java
+++ Steampunk_Game/src/team/tmbe/model/livingThing/nonHuman/NonHuman.java
@@ -1,7 +1,9 @@
 package team.tmbe.model.livingThing.nonHuman;
 
 import team.tmbe.model.livingThing.LivingThing;
+import team.tmbe.model.mainThings.Name;
 
 public abstract class NonHuman extends LivingThing {
+	protected Name m_Name;
 
 }
