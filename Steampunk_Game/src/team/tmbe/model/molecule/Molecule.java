e77f30c09ed8032e6107f8f2bcac671c626bf41b
Loki <phalandra.mandragora@googlemail.com>
2015-11-19 15:45:43 +0100

    editing Moleculeclass

--- Steampunk_Game/src/team/tmbe/model/molecule/Molecule.java
+++ Steampunk_Game/src/team/tmbe/model/molecule/Molecule.java
@@ -1,11 +1,28 @@
 package team.tmbe.model.molecule;
 
+import java.util.ArrayList;
 import java.util.List;
-import javax.lang.model.util.Elements;
+
+import team.tmbe.model.atoms.Element;
 
 public class Molecule {
 
-    private List<Elements> m_ElementsOfMolecule;
-    private List<Molecule> m_SubMolecules;
+	private List<Element> m_ElementsOfMolecule;
+	private List<Molecule> m_SubMolecules;
+	private final String m_MoleculeName;
+
+	public Molecule(String m_MoleculeName) {
+		this.m_MoleculeName = m_MoleculeName;
+		this.m_ElementsOfMolecule = new ArrayList<>();
+		this.m_SubMolecules = new ArrayList<>();
+	}
+
+	public void addElement(Element newElement) {
+		this.m_ElementsOfMolecule.add(newElement);
+	}
+
+	public void addSubMolecule(Molecule newMolecule) {
+		this.m_SubMolecules.add(newMolecule);
+	}
 
 }
