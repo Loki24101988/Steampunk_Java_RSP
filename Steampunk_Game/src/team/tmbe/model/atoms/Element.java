02f23ad7fd930e0a56fadf09365f9b8680dfedfd
Loki <phalandra.mandragora@googlemail.com>
2015-11-19 13:34:08 +0100

    edited class Element.java

--- Steampunk_Game/src/team/tmbe/model/atoms/Element.java
+++ Steampunk_Game/src/team/tmbe/model/atoms/Element.java
@@ -2,13 +2,20 @@ package team.tmbe.model.atoms;
 
 import team.tmbe.model.unLivingThing.UnLivingThing;
 
+/**
+ * This class represents an element out of the perodic table.
+ * 
+ * @author loki
+ *
+ */
 public final class Element extends UnLivingThing {
 
-	protected String m_Name;
-	protected int m_NumberOfElectrons;
-	protected int m_NumberOfNeutrons;
-	protected int m_NumberOfPositrons;
-	protected boolean m_IsIsotope;
-	protected int m_OrderNumber;
-	protected boolean m_IsRadioactive;
+	private String m_Name;
+	private int m_NumberOfElectrons;
+	private int m_NumberOfNeutrons;
+	private int m_NumberOfPositrons;
+	private boolean m_IsIsotope;
+	private int m_OrderNumber;
+	private boolean m_IsRadioactive;
+	private EListOfAtoms m_AtomType;
 }
