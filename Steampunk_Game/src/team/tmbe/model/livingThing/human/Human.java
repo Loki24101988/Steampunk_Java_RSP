fd913942eab9ffcecd6bfdbab819b9cc7b671d32
Loki <phalandra.mandragora@googlemail.com>
2015-11-23 15:57:50 +0100

    editing abstract classes Human and Animal

--- Steampunk_Game/src/team/tmbe/model/livingThing/human/Human.java
+++ Steampunk_Game/src/team/tmbe/model/livingThing/human/Human.java
@@ -1,9 +1,12 @@
 package team.tmbe.model.livingThing.human;
 
+import team.tmbe.model.Thing;
 import team.tmbe.model.livingThing.bodyParts.Body;
 import team.tmbe.model.livingThing.bodyParts.Head;
+import team.tmbe.model.mainCharacterThings.Name;
 
-public abstract class Human {
+public abstract class Human extends Thing {
+	protected Name m_Name;
 	protected Head m_Head;
 	protected Body m_Body;
 
--- Steampunk_Game/src/team/tmbe/model/livingThing/nonHuman/Animal.java
+++ Steampunk_Game/src/team/tmbe/model/livingThing/nonHuman/Animal.java
@@ -2,8 +2,10 @@ package team.tmbe.model.livingThing.nonHuman;
 
 import team.tmbe.model.livingThing.bodyParts.Body;
 import team.tmbe.model.livingThing.bodyParts.Head;
+import team.tmbe.model.mainCharacterThings.Name;
 
 public abstract class Animal extends NonHuman {
+	protected Name m_Name;
 	protected Body m_Body;
 	protected Head m_Head;
 }
