bc642015512e2a51b6463db04120e52e2e56cd26
Loki24101988 <raphael.sues@mi.uni-luebeck.de>
2016-04-11 09:25:35 +0200

    setting the fields in the emelent class to final

--- Steampunk_Game/src/team/tmbe/model/atoms/Element.java
+++ Steampunk_Game/src/team/tmbe/model/atoms/Element.java
@@ -10,24 +10,32 @@ import team.tmbe.model.unLivingThing.UnLivingThing;
  */
 public final class Element extends UnLivingThing {
 
-	private String m_Name;
-	private int m_NumberOfElectrons;
-	private int m_NumberOfNeutrons;
-	private int m_NumberOfPositrons;
-	private boolean m_IsIsotope;
-	private int m_OrderNumber;
-	private boolean m_IsRadioactive;
-	private EListOfAtoms m_AtomType;
-	private int numberOfConnections;
+	private final String m_Name;
+	private final int m_NumberOfElectrons;
+	private final int m_NumberOfNeutrons;
+	private final int m_NumberOfPositrons;
+	private final boolean m_IsIsotope;
+	private final int m_OrderNumber;
+	private final boolean m_IsRadioactive;
+	private final EListOfAtoms m_AtomType;
+	private final int numberOfConnections;
     
+	public Element(String name, int numberOfElectrons, int numberOfNeutrons, int numberOfPositrons, boolean isIsotope,
+	        int orderNumber, boolean isRadioactive, EListOfAtoms atomType, int numberOfConnections) {
+	    this.m_Name = name;
+	    this.m_NumberOfElectrons = numberOfConnections;
+	    this.m_NumberOfNeutrons = numberOfNeutrons;
+	    this.m_NumberOfPositrons = numberOfPositrons;
+	    this.m_IsIsotope = isIsotope;
+	    this.m_OrderNumber = orderNumber;
+	    this.m_IsRadioactive = isRadioactive;
+	    this.m_AtomType = atomType;
+	    this.numberOfConnections = numberOfConnections;
+	}
+	
     public String getM_Name() {
     
         return m_Name;
-    }
-    
-    public void setM_Name(String m_Name) {
-    
-        this.m_Name = m_Name;
     }
     
     public int getM_NumberOfElectrons() {
@@ -35,19 +43,9 @@ public final class Element extends UnLivingThing {
         return m_NumberOfElectrons;
     }
     
-    public void setM_NumberOfElectrons(int m_NumberOfElectrons) {
-    
-        this.m_NumberOfElectrons = m_NumberOfElectrons;
-    }
-    
     public int getM_NumberOfNeutrons() {
     
         return m_NumberOfNeutrons;
-    }
-    
-    public void setM_NumberOfNeutrons(int m_NumberOfNeutrons) {
-    
-        this.m_NumberOfNeutrons = m_NumberOfNeutrons;
     }
     
     public int getM_NumberOfPositrons() {
@@ -55,19 +53,9 @@ public final class Element extends UnLivingThing {
         return m_NumberOfPositrons;
     }
     
-    public void setM_NumberOfPositrons(int m_NumberOfPositrons) {
-    
-        this.m_NumberOfPositrons = m_NumberOfPositrons;
-    }
-    
     public boolean isM_IsIsotope() {
     
         return m_IsIsotope;
-    }
-    
-    public void setM_IsIsotope(boolean m_IsIsotope) {
-    
-        this.m_IsIsotope = m_IsIsotope;
     }
     
     public int getM_OrderNumber() {
@@ -75,19 +63,9 @@ public final class Element extends UnLivingThing {
         return m_OrderNumber;
     }
     
-    public void setM_OrderNumber(int m_OrderNumber) {
-    
-        this.m_OrderNumber = m_OrderNumber;
-    }
-    
     public boolean isM_IsRadioactive() {
     
         return m_IsRadioactive;
-    }
-    
-    public void setM_IsRadioactive(boolean m_IsRadioactive) {
-    
-        this.m_IsRadioactive = m_IsRadioactive;
     }
     
     public EListOfAtoms getM_AtomType() {
@@ -95,20 +73,12 @@ public final class Element extends UnLivingThing {
         return m_AtomType;
     }
     
-    public void setM_AtomType(EListOfAtoms m_AtomType) {
-    
-        this.m_AtomType = m_AtomType;
-    }
-    
     public int getNumberOfConnections() {
     
         return numberOfConnections;
     }
     
-    public void setNumberOfConnections(int numberOfConnections) {
     
-        this.numberOfConnections = numberOfConnections;
-    }
 	
 	
 }
